0794e7a47a829fba8a802376d8d24cd1b865d15f
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2022-10-27 17:59:57 +0530

    Add files via upload

---
+++ Page1/Page1.js
@@ -0,0 +1,44 @@
+// navbar
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
+// carousel
+
+$(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+    0:{
+    items:1,
+    nav: false
+    },
+    600:{
+    items:2,
+    nav: false
+    },
+    1000:{
+    items:3,
+    nav: false
+    }
+    }
+    });
---
+++ signup/signup.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("#button");
+
+   function myfunction(){
+    let name= document.getElementById('getName').value;
+    let email= document.getElementById('getEmail').value;
+   
+    console.log(name);
+    console.log(email);
+    
+}
