2258c66b0fc3fe1cee0023a54fe90dcc75586dcb
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2022-10-28 17:42:29 +0530

    Add files via upload

---
+++ components/login/login.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector("#button");
+
+   function myfunction(){
+    
+    let email= document.getElementById('getEmail').value;
+    let password= document.getElementById('getPass').value;
+   
+   
+    console.log(email);
+    console.log(password);
+    
+}
